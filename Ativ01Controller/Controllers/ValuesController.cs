54efd9883c6e08cd389d71948b9511231e304f2b
Luiza Campello <lumcampello@gmail.com>
2022-08-29 14:53:14 -0300

    Add project files.

---
+++ Ativ01Controller/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ativ01Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public List<Person> userRepository { get; set; }
+
+        public ValuesController()
+        {
+            userRepository = new()
+            {
+                new Person("Mateus", "123654789-11", DateTime.Parse("09/01/2003")),
+                new Person("Abobora", "123654719-11", DateTime.Parse("09/01/2002")),
+                new Person("Luiza", "123654719-11", DateTime.Parse("09/01/1996"))
+            };
+        }
+
+        [HttpGet]
+        public List<Person> GetUserRepository()
+        {
+            return userRepository;
+        }
+
+        [HttpPost]
+
+        public Person AddPerson(Person newPerson)
+        {
+            userRepository.Add(newPerson);
+            return newPerson;
+        }
+
+        [HttpPut]
+
+        public List<Person> EditPersonInfo(int userIndex, Person changePerson)
+        {
+            List<Person> personChanges = new()
+            {
+                userRepository[userIndex],
+                changePerson
+            };
+
+            userRepository[userIndex] = changePerson;
+
+            return personChanges;
+        }
+
+        [HttpDelete]
+
+        public List<Person> DeletePerson(int userIndex)
+        {
+            userRepository.RemoveAt(userIndex);
+            return userRepository;
+        }
+    }
+}
---
+++ Ativ01Controller/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Ativ01Controller
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string name, string CPF, DateTime birthDate)
+        {
+            Name = name;
+            this.CPF = CPF;
+            BirthDate = birthDate;
+            Age = CalculateAge(birthDate);
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate > today.AddYears ( - age))
+            {
+                age--;
+            }      
+
+            return age;
+        }
+
+    }
+}
