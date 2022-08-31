71c496948dc4feb791c2bb0a66862dcd3cfa5309
Luiza Campello <lumcampello@gmail.com>
2022-08-31 15:20:16 -0300

    Atividade 02 | Web III - ControllerBase/ApiController/Decorators

--- Ativ01Controller/Controllers/ValuesController.cs
+++ Ativ01Controller/Controllers/ValuesController.cs
@@ -3,57 +3,122 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ativ01Controller.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class ValuesController : ControllerBase
     {
-        public List<Person> userRepository { get; set; }
+        public List<Person> UserRepository { get; set; }
 
         public ValuesController()
         {
-            userRepository = new()
+
+            UserRepository = new()
             {
-                new Person("Mateus", "123654789-11", DateTime.Parse("09/01/2003")),
-                new Person("Abobora", "123654719-11", DateTime.Parse("09/01/2002")),
-                new Person("Luiza", "123654719-11", DateTime.Parse("09/01/1996"))
+                new Person("Matheus", "12365478911", DateTime.Parse("05/06/2002")),
+                new Person("Mateus", "10624900614", DateTime.Parse("09/01/2003")),
+                new Person("Luiza", "12365471911", DateTime.Parse("09/01/1996"))
             };
         }
 
-        [HttpGet]
-        public List<Person> GetUserRepository()
+        [HttpGet("person/getUserRepository")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<List<Person>> GetUserRepository()
         {
-            return userRepository;
+            return Ok(UserRepository);
         }
 
-        [HttpPost]
+        [HttpPost("person/register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
 
-        public Person AddPerson(Person newPerson)
+        public ActionResult<Person> AddPerson(Person newPerson)
         {
-            userRepository.Add(newPerson);
-            return newPerson;
+            UserRepository.Add(newPerson);
+            return StatusCode(201, newPerson);
         }
 
-        [HttpPut]
+        [HttpPut("person/{userIndex}/editPersonInfoByIndex")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
 
-        public List<Person> EditPersonInfo(int userIndex, Person changePerson)
+        public ActionResult<List<Person>> EditPersonInfoByIndex(int userIndex, Person changePerson)
         {
+            if(userIndex >= UserRepository.Count || userIndex < 0)
+            {
+                return BadRequest();
+            }
+            
             List<Person> personChanges = new()
             {
-                userRepository[userIndex],
+                UserRepository[userIndex],
                 changePerson
             };
 
-            userRepository[userIndex] = changePerson;
+            UserRepository[userIndex] = changePerson;
 
-            return personChanges;
+            return Accepted(personChanges);
         }
 
-        [HttpDelete]
+        [HttpPut("person/{personCPF}/editPersonInfoByCPF")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
 
-        public List<Person> DeletePerson(int userIndex)
+        public ActionResult<List<Person>> EditPersonInfoByCPF(string personCPF, Person changePerson)
         {
-            userRepository.RemoveAt(userIndex);
-            return userRepository;
+            int indexOf = UserRepository.FindIndex(person => person.CPF == personCPF);
+
+            if (indexOf != -1)
+            {
+                List<Person> personChanges = new()
+                {
+                UserRepository[indexOf],
+                changePerson
+                };
+
+                UserRepository[indexOf] = changePerson;
+
+                return StatusCode(202, personChanges);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("person/{userIndex}/DeletePersonByIndex")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        public IActionResult DeletePersonByIndex(int userIndex)
+        {
+            if (userIndex >= UserRepository.Count || userIndex < 0)
+            {
+                return NotFound();
+            }
+
+            UserRepository.RemoveAt(userIndex);
+
+            return NoContent();
+        }
+
+        [HttpDelete("person/{personCPF}/DeletePersonByCPF")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult DeletePersonByCPF(string personCPF)
+        {
+            int indexOf = UserRepository.FindIndex(person => person.CPF == personCPF);
+
+            if (indexOf != -1)
+            {
+                UserRepository.RemoveAt(indexOf);
+                return NoContent();
+            }
+
+            return NotFound();
+            
         }
     }
 }
--- Ativ01Controller/Person.cs
+++ Ativ01Controller/Person.cs
@@ -1,10 +1,20 @@
-﻿namespace Ativ01Controller
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ativ01Controller
 {
     public class Person
     {
+        [Required(ErrorMessage = "Nome é obrigatório")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        [MaxLength(11, ErrorMessage = "CPF deve conter 11 caracteres")]
+        [MinLength(11, ErrorMessage = "CPF deve conter 11 caracteres")]
         public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Data de nascimento é obrigatória")]
         public DateTime BirthDate { get; set; }
+
         public int Age { get; set; }
 
 
